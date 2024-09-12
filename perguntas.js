847323760a98eae60cb2be5426e037b3d5e652d1
Felipemagrelo100 <142028534+Felipemagrelo100@users.noreply.github.com>
2024-09-12 11:51:47 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Cama',
+    'uso para colocar a cabecinha?',
+    'o traviseiro'
+)
+
+criaCartao(
+    'Pai',
+    'pia aonde ta teu pai?',
+    'ta comprando cigarro'
+)
+
+criaCartao(
+    'Na Casa Da Sua Mãe',
+    'Oque tem?',
+    'seu pai sem cueca'
+)
+
+criaCartao(
+    'Ligua',
+    'grande mais estranho?',
+    'sua bunda'
+)
